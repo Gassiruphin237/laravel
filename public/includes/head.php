a7d64369ea67c823dbac3e5a65e401482fc12d6d
Joe Codeur <joefaha1@gmail.com>
2022-04-26 13:17:46 +0100

    refactor: project code refactor

--- public/includes/head.php
+++
@@ -1,8 +0,0 @@
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title><?php echo request()->path(); ?></title>
-    <link rel="stylesheet" href="{{ asset('/css/app.css') }}">
-
-
-</head>
--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', function () {
     return view('home');
-});
-Route::get('/home', function () {
-    return view('home');
-});
+})->name('root');
+
 Route::get('/panier', function () {
     return view('panier');
-});
+})->name('cart');
 
